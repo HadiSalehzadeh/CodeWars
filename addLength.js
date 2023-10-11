ec45624fafa9c70788bc36abdf70bd40966567b7
Fazelhadi <salehzadehfazelhadi@gmail.com>
2023-10-11 15:23:06 +0430

    Completed today's coding challenge

--- addLength.js
+++ addLength.js
@@ -11,5 +11,3 @@ Note: String will have at least one element; words will always be separated by a
 */
 
 const addLength = str => str.split(' ').map(word => `${word} ${word.length}`)
-
-console.log(addLength('apple ban'))
