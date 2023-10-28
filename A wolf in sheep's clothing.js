654a12329fac64bfbed143262c194be1eb868664
Fazelhadi <salehzadehfazelhadi@gmail.com>
2023-10-28 21:41:21 +0430

    Day 20

---
+++ A wolf in sheep's clothing.js
@@ -0,0 +1,10 @@
+/*
+Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
+
+Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array:
+*/
+
+function warnTheSheep(queue) {
+    const position = queue.reverse().indexOf('wolf');
+    return position === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${ position }! You are about to be eaten by a wolf!`;
+}
