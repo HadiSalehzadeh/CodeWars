3ec110eb67164b11fbf682d17165b5ae978c70b6
Fazelhadi <salehzadehfazelhadi@gmail.com>
2023-11-01 14:47:03 +0430

    Day 24

---
+++ find the position.js
@@ -0,0 +1,15 @@
+/*
+When provided with a letter, return its position in the alphabet.
+
+Input :: "a"
+
+Ouput :: "Position of alphabet: 1"
+*/
+
+function position(letter){
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  const position = alphabet.indexOf(letter.toLowerCase()) + 1
+  console.log(position)
+}
+
+position('z')
