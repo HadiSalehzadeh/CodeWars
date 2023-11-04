05d7eb72d0d566b652996bed6c0bc1052a59474c
Fazelhadi <salehzadehfazelhadi@gmail.com>
2023-11-04 13:25:48 +0430

    Day 27

---
+++ alternatingCase.js
@@ -0,0 +1,20 @@
+/*
+Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. 
+*/
+
+String.prototype.toAlternatingCase = function() {
+  let newStr = '';
+  for (let i = 0; i < this.length; i++) {
+    this[i].toLowerCase() === this[i] ? newStr += this[i].toUpperCase() : newStr += this[i].toLowerCase();
+  }
+  return newStr;
+};
+
+
+
+
+console.log(toAlternatingCase("hello world"))
+console.log(toAlternatingCase("HELLO WORLD"))
+console.log(toAlternatingCase("hello WORLD"))
+console.log(toAlternatingCase("HeLLo WoRLD"))
+console.log(toAlternatingCase(12345))
