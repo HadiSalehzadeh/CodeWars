768765b3befd755e4999e510ade1a496ccf7f670
Fazelhadi <salehzadehfazelhadi@gmail.com>
2023-11-14 15:27:14 +0430

    Second coding challenge updated

--- remove_first_and_last_character.js
+++ remove_first_and_last_character.js
@@ -3,17 +3,14 @@
 // If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
 
 
-const array = (string) => {
-  if (string.length <= 2) {
-    return null
-  } else {
-    string = string.substring(1)
-    string = string.substring(0, string.length - 1)
-    let newStr = string.split(", ")
-    return newStr.join(" ")
-  }
+function array(arr){
+  return arr.split(",").slice(1,-1).join(" ") || null;
 }
 
-
-
-console.log(array('1, 2, 3, 4, 5'))
+console.log(array((''))) //null
+console.log(array(('1'))) //null
+console.log(array(('A1,B2'))) //null
+console.log(array(('1,2,3'))) //'2'
+console.log(array(('1,2,3,4'))) //'2 3'
+console.log(array(('A1,B2,C3,D4,E5'))) //'B2 C3 D4'
+console.log(array(('A,1,23,456,78,9,Z'))) //'1 23 456 78 9'
