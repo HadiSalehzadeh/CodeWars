ae3c7b350616b23652599a5b840f2712490449bc
Fazelhadi <salehzadehfazelhadi@gmail.com>
2023-11-29 14:00:48 +0430

    Completed today's second coding challenge

---
+++ unpaired_items.js
@@ -0,0 +1,22 @@
+// Write a function that takes two array arguments, and returns a new array populated with the elements that appear in either array, but not in both. Elements should only appear once in the returned array.
+// The order of the elements in the result should follow what appears in the first array, then the second one.
+
+// Examples
+// [1, 2, 3, 3], [3, 2, 1, 4, 5]) --> [4, 5]
+// ["tartar", "blanket", "cinnamon"], ["cinnamon", "blanket", "domino"] --> ["tartar", "domino"]
+// [77, "ciao"], [78, 42, "ciao"] --> [77, 78, 42]
+// [1, 2, 3, 3], [3, 2, 1, 4, 5, 4] --> [4,5]
+// [1, 2, 3] , [3, 3, 2, 1] --> []
+
+function hotSingles(arr1, arr2) {
+  let newArr = []
+  for (i = 0; i < arr1.length; i++) {
+    if (!arr2.includes(arr1[i]) && !newArr.includes(arr1[i])) newArr.push(arr1[i])
+  }
+  for (i = 0; i < arr2.length; i++) {
+    if (!arr1.includes(arr2[i]) && !newArr.includes(arr2[i])) newArr.push(arr2[i])
+  }
+  return newArr
+}
+
+console.log(hotSingles([1, 2, 3, 3], []))
