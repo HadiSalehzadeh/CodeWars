6574cc3caa067d335e08f93ec7f97714f4b808cc
Fazelhadi <salehzadehfazelhadi@gmail.com>
2023-12-09 15:15:38 +0430

    Completed today's second coding challenge

---
+++ Functions-of-Integers-on-Cartesian-Plane.js
@@ -0,0 +1,9 @@
+function sumin(n) {
+  return n * n * n / 3 + n * n / 2 + n / 6;
+}
+function sumax(n) {
+  return 2 * n * n * n / 3 + n * n / 2 - n / 6;
+}
+function sumsum(n) {
+  return n * n * n + n * n;
+}
