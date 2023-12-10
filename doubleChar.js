32bed640ef2106b0aed7153d4f7b2e53029e3167
Fazelhadi <salehzadehfazelhadi@gmail.com>
2023-12-10 14:05:56 +0430

    completed today's coding challenge

--- doubleChar.js
+++ doubleChar.js
@@ -9,10 +9,3 @@ const doubleChar = str => {
   }
   return newStr
 }
-
-console.log(doubleChar("abcd"))
-console.log(doubleChar("Adidas"))
-console.log(doubleChar("1337"))
-console.log(doubleChar("illuminati"))
-console.log(doubleChar("123456"))
-console.log(doubleChar("%^&*("))
