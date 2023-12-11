6f397be0b46bf81764427c074d8783ff152cb367
Fazelhadi <salehzadehfazelhadi@gmail.com>
2023-12-11 13:57:54 +0430

    Completed today's third coding challenge

---
+++ Excessively-Abundant-Numbers.js
@@ -0,0 +1,15 @@
+// An abundant number or excessive number is a number for which the sum of its proper divisors is greater than the number itself.
+
+// The integer 12 is the first abundant number. Its proper divisors are 1, 2, 3, 4 and 6 for a total of 16 (> 12).
+
+// Derive function abundantNumber(num)/abundant_number(num) which returns true/True/.true. if num is abundant, false/False/.false. if not.
+
+function abundantNumber(num) {
+  let sum = 0;
+  for (let i = 1; i <= num / 2; i++) {
+    if (num % i === 0) {
+      sum += i;
+    }
+  }
+  return sum > num;
+}
