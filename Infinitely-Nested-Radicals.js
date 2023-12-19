e19275677a7d9e4aec00fca38d5fb3d4e21b23f6
Fazelhadi <salehzadehfazelhadi@gmail.com>
2023-12-19 14:26:30 +0430

    Completed today's coding challenge

--- Infinitely-Nested-Radicals.js
+++ Infinitely-Nested-Radicals.js
@@ -5,3 +5,11 @@
 // for a given value x
 // Simple!
 // arguments passed in will be 1 or greater
+
+function evaluateRadicals(x) {
+  if (x < 1) {
+    return x;
+  } else {
+    return Math.sqrt(evaluateRadicals(x - 1));
+  }
+}
