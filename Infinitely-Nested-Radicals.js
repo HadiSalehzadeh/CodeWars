b6cfa777b5f08b05e0c26bf1c9dc6c7c8b663911
Fazelhadi <salehzadehfazelhadi@gmail.com>
2023-12-19 14:24:09 +0430

    Completed today's coding challenge

---
+++ Infinitely-Nested-Radicals.js
@@ -0,0 +1,13 @@
+Hello,
+
+I am Jomo Pipi
+
+and today we will be evaluating an expression like this:
+
+(there are an infinite number of radicals)
+
+for a given value x
+
+Simple!
+
+arguments passed in will be 1 or greater
