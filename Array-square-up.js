2756edab549dfb40b7c17d75681a4ab684418949
Fazelhadi <salehzadehfazelhadi@gmail.com>
2023-12-23 15:38:50 +0430

    Completed today's coding challenge

--- Array-square-up.js
+++ Array-square-up.js
@@ -4,3 +4,17 @@
 // squareUp(2) => [0, 1, 2, 1]
 // squareUp(4) => [0, 0, 0, 1, 0, 0, 2, 1, 0, 3, 2, 1, 4, 3, 2, 1]
 // n<=1000.
+
+function squareUp(n) {
+  let result = [];
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (j <= n - i) {
+        result.push(0);
+      } else {
+        result.push(n - j + 1);
+      }
+    }
+  }
+  return result;
+}
