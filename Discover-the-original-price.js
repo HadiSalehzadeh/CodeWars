5651e5dffbe9e04d586af67ba92ba5d87e1fb923
Fazelhadi <salehzadehfazelhadi@gmail.com>
2023-12-25 14:40:00 +0430

    Completed today's coding challenge

--- Discover-the-original-price.js
+++ Discover-the-original-price.js
@@ -4,3 +4,4 @@
 // Given an item at $75 sale price after applying a 25% discount, the function should return the original price of that item before applying the sale percentage, which is ($100.00) of course, rounded to two decimal places.
 // DiscoverOriginalPrice(75, 25) => 100.00M where 75 is the sale price (discounted price), 25 is the sale percentage and 100 is the original price
 
+const discoverOriginalPrice = (discountedPrice, salePercentage) => parseFloat((discountedPrice / (1 - (salePercentage / 100))).toFixed(2));
