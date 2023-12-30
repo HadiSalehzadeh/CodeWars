be10cb5874a6f4226703973186a90c4b118f9330
Fazelhadi <salehzadehfazelhadi@gmail.com>
2023-12-30 15:49:59 +0430

    Completed today's coding challenge

--- Power-of-4.js
+++ Power-of-4.js
@@ -1,7 +1,5 @@
 // Write a method that returns true if a given parameter is a power of 4, and false if it's not. If parameter is not an Integer (eg String, Array) method should return false as well.
-
 // (In C# Integer means all integer Types like Int16,Int32,.....)
-
 // Examples
 // powerOf4(1024) // returns true
 // powerOf4(44) // returns false
