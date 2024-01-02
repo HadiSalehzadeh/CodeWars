ea232bb8eb644081f021af81e819a130c356a39f
Fazelhadi <salehzadehfazelhadi@gmail.com>
2024-01-02 14:14:23 +0430

    completed today's coding challenge

--- Half-life.js
+++ Half-life.js
@@ -1,8 +1,6 @@
 // The half-life of a radioactive substance is the time it takes (on average) for one-half of its atoms to undergo radioactive decay.
-
 // Task Overview
 // Given the initial quantity of a radioactive substance, the quantity remaining after a given period of time, and the period of time, return the half life of that substance.
-
 // Usage Examples
 // // if quantity halves in 1 time period, half-life = 1
 // halfLife(10, 5, 1) => 1
