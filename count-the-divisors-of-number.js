cc1b49bde8f15d19bf69ad8c7f5f15884a195a54
Fazelhadi <salehzadehfazelhadi@gmail.com>
2024-01-04 15:18:58 +0430

    completed today's coding challenge

--- count-the-divisors-of-number.js
+++ count-the-divisors-of-number.js
@@ -6,3 +6,17 @@
 // 12 --> 6 // we have 6 divisors - 1, 2, 3, 4, 6 and 12
 // 30 --> 8 // we have 8 divisors - 1, 2, 3, 5, 6, 10, 15 and 30
 // Note you should only return a number, the count of divisors. The numbers between parentheses are shown only for you to see which numbers are counted in each case.
+
+function getDivisorsCnt(n) {
+  let count = 0;
+  for (let i = 1; i * i <= n; i++) {
+    if (n % i === 0) {
+      if (i * i === n) {
+        count++;
+      } else {
+        count += 2;
+      }
+    }
+  }
+  return count;
+}
