ddb8a6f601be599270e33bc7528f1456f16c2ba0
Fazelhadi <salehzadehfazelhadi@gmail.com>
2024-01-06 16:21:25 +0430

    completed today's coding challenge

--- Dont-give-me-five.js
+++ Dont-give-me-five.js
@@ -10,3 +10,13 @@
 // I'm very curious for your solutions and the way you solve it. Maybe someone of you will find an easy pure mathematics solution.
 // Have fun coding it and please don't forget to vote and rank this kata! :-)
 // I have also created other katas. Take a look if you enjoyed this kata!
+
+function dontGiveMeFive(start, end) {
+  let count = 0;
+  for (let i = start; i <= end; i++) {
+    if (i.toString().includes('5') === false) {
+      count++;
+    }
+  }
+  return count;
+}
